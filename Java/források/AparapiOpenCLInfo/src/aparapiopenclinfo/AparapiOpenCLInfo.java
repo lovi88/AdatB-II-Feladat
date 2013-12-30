61e4ad1c0dd7ea129b810391cef47206a96d5abb
lovi88 <l.pista@hotmail.com>
2013-12-30 19:46:50 +0100

    Fájlok kezelése bekerűlt java + pl/sql oldalon, doksiba is.

--- Java/források/AparapiOpenCLInfo/src/aparapiopenclinfo/AparapiOpenCLInfo.java
+++ Java/források/AparapiOpenCLInfo/src/aparapiopenclinfo/AparapiOpenCLInfo.java
@@ -1,11 +1,13 @@
 package aparapiopenclinfo;
 
-//import java.io.File;
-import aparapiopenclinfo.dbconnection.DataManager;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.sql.SQLException;
-import javax.persistence.EntityManager;
-import oracle.jdbc.pool.OracleConnectionPoolDataSource;
-import oracle.jdbc.pool.OracleDataSource;
+
 
 //import java.io.IOException;
 /**
@@ -17,26 +19,43 @@ public class AparapiOpenCLInfo {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
-//        File f = new File("alma.txt");
-//        try {
-//            f.createNewFile();
-//        } catch (IOException ex) {
-//            System.out.println("filelétrehozási probléma");
-//        }
-//        DataManager dm = new DataManager();
-//        EntityManager em = dm.getEntityManager();
-//        
+
+        File f = new File("C:\\LJava\\OCLInfo.txt");
+        
+        Charset ASCII_HU = Charset.forName("ISO-8859-2");
+        
+        OutputStream os = null;
+        OutputStreamWriter ow = null;
+        try {
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+
+            os = new FileOutputStream(f);
+            ow = new OutputStreamWriter(os, ASCII_HU);
+
+            ow.write(getOpenCLInfo());
+
+        } catch (Exception e) {
+        } finally {
+            try {
+                if (ow != null) {
+                    ow.close();
+                }
+
+                if (os != null) {
+                    os.close();
+                }
+
+            } catch (IOException ex) {
+            }
+        }
+
     }
 
-    static void MyConnection() throws SQLException {
-
-        String user;
-        String password;
-        String database;
-
-        OracleDataSource ods = new OracleDataSource();
-        
-        ods.setURL("jdbc:oracle:");
+    static String getOpenCLInfo() {
+        return "<div><h2>Az Aparapi által érzékelt OpenCl eszközök</h2><p>Legjobb eszköz: <br />Device 5233328 type:GPU maxComputeUnits=14 maxWorkItemDimensions=3 maxWorkItemSizes={256, 256, 256}   maxWorkWorkGroupSize=256  globalMemSize=2147483648  localMemSize=32768</p><p>Elsődleges CPU: <br />Device 227840384  type:CPU  maxComputeUnits=6  maxWorkItemDimensions=3  maxWorkItemSizes={1024, 1024, 1024}  maxWorkWorkGroupSize=1024  globalMemSize=8586293248  localMemSize=32768</p><p>Elsődleges GPU: <br />Device 5233328  type:GPU  maxComputeUnits=14  maxWorkItemDimensions=3  maxWorkItemSizes={256, 256, 256}  maxWorkWorkGroupSize=256  globalMemSize=2147483648  localMemSize=32768</p></div>";
     }
 }
